64ee66059c3e718c9bbe1c3f1081a77d696ce050
7khalil <Meftah.khalil@live.fr>
2016-07-15 19:57:30 +0000

    change

--- Assets/Scripts/Clamp.cs
+++ Assets/Scripts/Clamp.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class Clamp : MonoBehaviour {
 
+
 	void OnTriggerExit(Collider other)
 	{
 		if(other.gameObject.tag == "Player"){
 			Debug.Log ("the player Exit from the PlayGround !");
 		}
+
 	}
 }
