d1fc0b448a912851b1de2d50aeeec6b29bc99375
7khalil <Meftah.khalil@live.fr>
2016-07-17 14:12:13 +0000

    Revert "change"
    This reverts commit 64ee66059c3e718c9bbe1c3f1081a77d696ce050.

--- Assets/Scripts/Clamp.cs
+++ Assets/Scripts/Clamp.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class Clamp : MonoBehaviour {
 
-
 	void OnTriggerExit(Collider other)
 	{
 		if(other.gameObject.tag == "Player"){
 			Debug.Log ("the player Exit from the PlayGround !");
 			Debug.Log ("GitHub Test");
 		}
-
 	}
 }
